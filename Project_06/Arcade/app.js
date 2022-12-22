7925fe39214525d8be8b25fb4f5f587790976f6a
Richierichman15 <richierichman15@gmail.com>
2022-12-22 14:48:45 -0600

    Project tik tak toe started

---
+++ Project_06/Arcade/app.js
@@ -0,0 +1,63 @@
+//STATE
+let state;
+
+const gameState = {
+    players: ['x', 'o'],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ],
+    currentPlayer: ['']
+}
+
+const buildInitialState = () => {
+
+};
+
+
+// render
+const renderState = () => {
+
+};
+
+
+
+// DOM SELECTORS
+
+
+
+
+
+
+// HELPER FUNCTIONS
+
+
+
+
+
+
+
+// EVENT LISTENERS
+const onBoardClick = () => {
+
+    const tick = () => {
+
+    renderState();
+}}
+
+setInterval(tick, 1000 / 30)
+
+document.addEventListener('keydown', (event) => {
+
+});
+
+const board = document.getElementById('board');
+board.addEventListener('click', (event) => {
+
+});
+
+
+
+
+// INITIAL SETUP
