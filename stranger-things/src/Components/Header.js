af9996e48d4aa05bfb9a069b139e483974586612
Richierichman15 <richierichman15@gmail.com>
2023-02-28 15:04:26 -0600

    test again

--- stranger-things/src/Components/Header.js
+++ stranger-things/src/Components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <>
         <nav>
             <h1 className="header">Stranger Things</h1>
-            {/* <Link to='/' className="header">Home</Link> */}
+            <Link to='/' className="header">Home</Link>
             <Link to='/posts' className="header">Posts </Link>
             <Link to='/login' className="header" >Login/Register</Link>
             <Link to='/profile' className="header" >Profile</Link>
